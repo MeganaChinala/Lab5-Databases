1480813965b96b8bb795fe09f842bcccb25d2ccd
MeganaChinala <m.chinalachaiagari@gmail.com>
2019-04-01 11:20:35 -0500

    trying to change to make it work

--- DeletePost.php
+++ DeletePost.php
@@ -3,15 +3,14 @@ $DB = new mysqli("mysql.eecs.ku.edu", "m954c512", "yiemeKu7", "m954c512");
 $array = $_POST["post"];
 for ($i = 0; $i < count($array); $i++)
 {
-	//delete post
 	$query = "DELETE FROM Posts WHERE post_id='$array[$i]'";
 	if ($DB->query($query))
 	{
-		echo "Post #" . $array[$i] . " has been successfully deleted<br>";
+		echo "Post #" . $array[$i] . " deleted<br>";
 	}
 	else
 	{
-		echo "Error deleting post #" . $array[$i] . "<br>";
+		echo "Unable to delete Post #" . $array[$i] . "<br>";
 	}
 }
 $DB->close();
